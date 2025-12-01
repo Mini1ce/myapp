8e4588a8d4cca5f0886a6b1532315c8146039ce0
nanithe02 <nanithe02@gmail.com>
2025-12-01 08:31:54 +0000

    RecipeListDesign

---
+++ lib/model/recipe.dart
@@ -0,0 +1,13 @@
+class Recipe {
+  String imgLabel;
+  String imageUrl;
+
+  Recipe(this.imgLabel , this.imageUrl);
+
+static List<Recipe> samples =[
+Recipe('assets/image/GC.webp', 'Perk up your midweek meal with flaky chunks of fish cooked in creamy coconut milk and mild Thai green curry paste, ready in just 10 minutes.'),
+Recipe('assets/image/salad(yum nua seua ronghai).webp', 'Crying tiger beef salad (yum nua seua rong hai)'),
+Recipe('assets/image/pum.webp', 'Make the most of seasonal pumpkins or butternut squash in this Thai curry, flavoured with dill, fish sauce and homemade red and green chilli paste.'),
+Recipe('assets/image/padthai.webp', 'Pad Thai'),
+];
+}
